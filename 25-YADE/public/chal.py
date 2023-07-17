ecf649d37a367f85a35264892d9d76eaf745f7a0
Gareth Ma <grhkm21@gmail.com>
2023-07-17 03:37:42 +0300

    Challenge 25 - YADE (Yet Another Diophantine Equation)
    Actually deployable, might put bounty on this

--- 25-YADE/public/chal.py
+++ 25-YADE/public/chal.py
@@ -11,6 +11,7 @@ seen = set()
 for _ in range(1000):
     a, b, c = [int(input(": ")) for _ in range(3)]
     assert all(2**2048 < abs(d) < 10**4300 for d in [a, b, c])
+    assert (a, b, c) not in seen
     assert d(a, b, c) == 0
     seen.add((a, b, c))
 
