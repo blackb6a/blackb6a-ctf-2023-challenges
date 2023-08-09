875e6f7f56fb14c279d57e43ba34d1b3cfae8103
MesAyaneru <hollowctf@gmail.com>
2023-08-09 21:36:57 +0000

    update challenge 18

---
+++ 18-amnesia/README.md
@@ -0,0 +1,26 @@
+# 最後一個糊塗神 / Amnesia
+
+## Category
+
+Web / Crypto
+
+## Contributors
+
+-   **Author:** hollow, Mystiz
+-   **Testers:** 
+
+## Description
+
+> 是我丟失千斤記憶 
+> 你所有秘密留在昨日
+
+https://www.youtube.com/watch?v=Mvec1GN_dtY
+
+
+
+## Note
+
+
+## Flag
+
+b6actf{h4v1ng_b64ctf_1n_my_pub1c_key_m4d5_m5_s0b5r}
---
+++ 18-amnesia/env/src/app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, Response, request
+import secrets
+from werkzeug.exceptions import HTTPException
+import json
+import os
+import jwt
+
+
+app = Flask(__name__)
+app.secret_key = secrets.token_bytes()
+
+f = open("jwks.json")
+jwks_content = f.read()
+f.close()
+
+FLAG = os.getenv("FLAG")
+
+valid_issuer_domain = os.getenv("HOST")
+valid_algo = "RS256"
+
+def is_valid_algo(token):
+    headers = jwt.get_unverified_header(token)
+    algo = headers["alg"]
+    return algo == valid_algo
+
+def authorize_token(token):
+    if not is_valid_algo(token):
+        raise Exception(f"Only {valid_algo} is allowed.")
+    public_key = json.loads(jwks_content)["keys"][0]["x5c"][0]
+    public_key = f"-----BEGIN PUBLIC KEY-----\n{public_key}\n-----END PUBLIC KEY-----"
+    decoded_token = jwt.decode(token, public_key, algorithms=[valid_algo])
+    if "user" not in decoded_token:
+        raise Exception("\"user\" claim required")
+    if decoded_token["user"] == "memory hero":
+        return True
+    return False
+
+@app.after_request
+def after_request_callback(response: Response):
+    print(response.__dict__)
+    if response.headers["Content-Type"].startswith("text/html"):
+        updated = render_template("index.html", status=response.status_code, message=response.response[0].decode())
+        response.set_data(updated)
+    if "auth" not in request.cookies:
+        response.set_cookie("auth", "asdf")
+    return response
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    if isinstance(e, HTTPException):
+        return e
+    return str(e), 500
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def index(path):
+    return "Flag at /flag", 200
+
+@app.route("/.well-known/jwks.json")
+def jwks():
+    return jwks_content, 200, {"Content-Type", "application/json"}
+
+@app.route("/flag")
+def flag():
+    if "auth" not in request.cookies:
+        raise Exception("Authorization cookie required.")
+    if not authorize_token(request.cookies.get("auth")):
+        return "Not authorized. Only memory hero can get the flag.", 403
+    else:
+        return f"Welcome! The flag is {FLAG}", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="3000")
